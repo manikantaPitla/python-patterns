efacc27e0379e63d912204ca5494e84d53a7bdc0
manikantaPitla <pitlamanikanta81@gmail.com>
2023-09-29 20:00:57 +0530

    minor Changes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,23 +5,22 @@ const About = () => {
     <div className="about-container">
       <h1 className="heading">About</h1>
       <p className="para">
-        It is a website dedicated to Python printing patterns! Our goal is to
+        It is a website dedicated to Python printing patterns! My goal is to
         provide a fun and educational resource for people who are interested in
         learning how to create beautiful designs.
       </p>
       <p className="para">
-        Our team is made up of passionate Python programmers who are excited to
-        share their knowledge and expertise with others. We believe that Python
-        printing patterns are a great way to learn programming concepts like
-        loops, conditionals, and functions, while also exploring your creativity
-        and imagination.
+        I'm excited to share my knowledge and expertise with others. I have
+        believe that Python printing patterns are a great way to learn
+        programming concepts like loops, conditionals, and functions, while also
+        exploring your creativity and imagination.
       </p>
       <p className="para">
-        We created this website because we saw a need for a comprehensive
+        I've created this website because i saw a need for a comprehensive
         resource that would guide users through the process of creating
         different types of patterns, from simple shapes to more complex designs.
-        Our tutorials and code samples are designed to be accessible and easy to
-        follow, even for beginners who are new to programming.
+        Code samples are designed to be accessible and easy to follow, even for
+        beginners who are new to programming.
       </p>
     </div>
   );
--- src/components/PatternItem/index.js
+++ src/components/PatternItem/index.js
@@ -12,13 +12,22 @@ const PatternItem = (props) => {
       <p className="q-text">{codeDescription}</p>
       <p className="q-text">{codeHint}</p>
       <div className="row">
-        <div className="col-12 py-2">
+        <div className="col-12 py-2 mb-3  position-relative">
+          <div className="code-ground-title-container  shadow-1 d-flex justify-content-center align-items-center">
+            <p className="para code-g-text m-0">Input</p>
+          </div>
           <CodeMirrorEditor className="input-code-container" code={codeInput} />
         </div>
-        <div className="col-12 col-md-6 py-2 mb-3 mb-md-0 code-and-output">
+        <div className="col-12 col-md-6  py-2 mb-4 mb-md-0 code-and-output  position-relative">
+          <div className="code-ground-title-container shadow-1 d-flex justify-content-center align-items-center">
+            <p className="para code-g-text m-0">Code</p>
+          </div>
           <CodeMirrorEditor code={code} />
         </div>
-        <div className="col-12 col-md-6 py-2 code-and-output">
+        <div className="col-12 col-md-6 mb-3 py-2 code-and-output  position-relative">
+          <div className="code-ground-title-container shadow-1 d-flex justify-content-center align-items-center">
+            <p className="para code-g-text m-0">Output</p>
+          </div>
           <CodeMirrorEditor code={codeOutput} />
         </div>
       </div>
